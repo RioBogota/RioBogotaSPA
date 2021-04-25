8972f4298676c8152c92e658879ab4d44880d388
andress19 <monosony_101@hotmail.com>
2021-04-25 18:42:05 -0500

    Permitir consulta de ordenes a los usuarios

--- src/app/orden/respuesta/consulta/consulta.component.ts
+++ src/app/orden/respuesta/consulta/consulta.component.ts
@@ -16,6 +16,9 @@ export class ConsultaComponent extends Base implements OnInit {
   respuestas = [];
   ordenes = [];
   cargando = false;
+  tipoUsuario: any;
+  acceso: boolean;
+  usuario: any;
   p: number = 1;
   constructor(
     private ordenService: OrdenService,
@@ -28,6 +31,23 @@ export class ConsultaComponent extends Base implements OnInit {
   ngOnInit() {
     this.selectedMunicipio = -1;
     this.selectedOrden = -1;
+    this.usuario = sessionStorage.usuario ? JSON.parse(sessionStorage.usuario) : undefined;
+    if(!this.usuario) {
+      this.appService.error('No tiene permisos para ingresar a la pagina. Consulte al administrador.');
+      this.acceso = false;
+      return;
+    }
+    this.unsubscribeOndestroy(this.ordenService.isValidUser(this.usuario.usuario1)
+    .subscribe(data => {
+      this.tipoUsuario = data; 
+      this.acceso = data > 0;
+      if (!this.acceso) {
+        this.appService.error('No tiene permisos para ingresar a la pantalla, consulte al adminstrador');
+      }
+    }, err => {
+      this.acceso = false;
+      this.appService.error('Se produjo un error al cargar la pagina intente nuevamente');
+    }));
     this.unsubscribeOndestroy(
       this.ordenService.getMunicipios().subscribe(
         (data) => (this.municipios = data),
@@ -50,13 +70,15 @@ export class ConsultaComponent extends Base implements OnInit {
     this.cargando = true;
     this.unsubscribeOndestroy(
       this.ordenService
-        .getRespuestas(this.selectedMunicipio, this.selectedOrden)
+        .getRespuestas(this.selectedMunicipio, this.selectedOrden, this.tipoUsuario === 1 ? 'NONE' : this.usuario.usuario1)
         .subscribe((data) => {
           this.cargando = false;
           this.respuestas = data.map(respuesta => {
           respuesta.orden = this.ordenes.find(orden => orden.idOrden === respuesta.idPreguntaNavigation.idOrden)
           return respuesta;
-        })})
+        })}, err => {
+          this.appService.error('Se produjo un error al consultar las preguntas intente nuevamente.');
+        })
     );
   }
 
@@ -83,7 +105,6 @@ export class ConsultaComponent extends Base implements OnInit {
             });
           },
           (error) => {
-            console.error(error);
             this.appService.error(
               "Se produjo un error al cargar el archivo."
             );
